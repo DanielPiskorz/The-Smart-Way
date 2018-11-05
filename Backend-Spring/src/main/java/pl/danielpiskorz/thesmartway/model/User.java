ac6519613f5c280c2db38ad365bd7d6bfad2cb70
DanielPiskorz <eel5426@gmail.com>
2018-11-05 21:42:11 +0100

    User authentication model classes

---
+++ Backend-Spring/src/main/java/pl/danielpiskorz/thesmartway/model/User.java
@@ -0,0 +1,77 @@
+package pl.danielpiskorz.thesmartway.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class User {
+
+	private Long id;
+
+	private String name;
+
+	private String username;
+
+	private String email;
+
+	private String password;
+
+	private Set<Role> roles = new HashSet<>();
+
+	public User() {
+	}
+
+	public User(String name, String username, String email, String password) {
+		this.name = name;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+}
