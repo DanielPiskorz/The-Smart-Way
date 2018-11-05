c78226dda12b5691b63467e9805ab19f7780e730
DanielPiskorz <eel5426@gmail.com>
2018-11-05 21:25:53 +0100

    Initial Spring project with dependencies

---
+++ Backend-Spring/src/test/java/pl/danielpiskorz/thesmartway/BackendSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.danielpiskorz.thesmartway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
